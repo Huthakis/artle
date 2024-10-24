cc12d8f65c408c4c1306113f148b10495e1e47e6
Huthakis <109891233+Huthakis@users.noreply.github.com>
2024-10-25 11:41:11 +1300

    Updated code to remove need for two endgame pages

--- README.md
+++ README.md
@@ -1,24 +1,22 @@
 # artle
 #### Video Demo: [Link to video demo] (URL HERE)
 #### Description:
-A wordle type clone for art and art prices. On each launch, the user is presented with a piece of artwork and given up to 6 rounds to correctly guess its price (to within 10%). After each guess, the user will be told if their guess was correct, too high, or too low. If the guess if not correct they will also be given a clue about the auction, painting, or artist.
+A wordle type clone for art and art prices. On each launch, the user is presented with a piece of artwork and given up to 6 rounds to correctly guess its price (to within 5%). After each guess, the user will be told if their guess was correct, too high, or too low. If the guess if not correct they will also be given a clue about the auction, painting, or artist.
 
 This web application uses 7 files:
 1. app.py
 	- Backend application code, there is setup code and two app routes in this file:
 		- The setup code loads the art data file and randomly selects a row to be used for the game
 		- app.route('/') contains logic for rendering the index.html page until the user is out of rounds, then the over.html page is rendered
-		- app.route('/guess') contains logic for checking the guess against the art price, renders the winner.html page if the guess is correct (to within 10%), otherwise returns the too high/low result, increments the round counter, and redirects to app.route('/')
+		- app.route('/guess') contains logic for checking the guess against the art price, renders the winner.html page if the guess is correct (to within 5%), otherwise returns the too high/low result, increments the round counter, and redirects to app.route('/')
 2. art_data.csv
-	- Data about the artwork, contains multiple columns of information, including a link to an online image of the artwork. Modified from the original source by removing duplicates, artists with only one piece of artwork, art without data for estimates, Chinese characters, and reformatted text to title case
+	- Data about the artwork, contains multiple columns of information, including a link to an online image of the artwork. Modified from the original source by removing duplicates, artists with only one piece of artwork, art without data for auction estimates, Chinese characters, and reformatted text to title case
 4. static/styles.css
 	- CSS style page to maintain consistent styling between pages.
 5. index.html
 	- Landing page for the app and where the user will make their guesses, above the guess box is a table that will populate with the guesses from the previous rounds and the outcome (too high or too low). Below the guess box is a table that will populate with the clues after each incorrect guess
-6. winner.html
-	- Display page for when the user guesses the price correctly (to within 10%), provides all information from the clues
-7. over.html
-	- Display page for when the user is out of rounds and the price has not been guessed correctly, provides all information from the clues
+6. over.html
+	- Display page for when the user has correct guessed the price (to within 5%), or is out of rounds and the price has not been guessed correctly, provides all information from the clues
 
 This project was inspired a YouTube video from Wendover Productions called "The Art Market is a Scam (And Rich People Run It). The video covers how some art is auctioned for very large sums, then discusses how these prices are manipulated by purchasers and dealers. The idea behind creating this web application was to reinforce how difficult it is to put a price on art when minimal information is provided.
 
--- app.py
+++ app.py
@@ -12,40 +12,48 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 # Load data
 art_df = pd.read_csv('art_data.csv')
 
+
+# Initalise global variables
+tolerance = 0.05
+counter = 1
+clues = []
+outcomes = {}
+
+
 # Set random number
 rand_number = randint(0, len(art_df) + 1)
-
 # Get information on random art
 artist = art_df.artist.iloc[rand_number]
 art_name = art_df.title.iloc[rand_number]
 art_url = art_df.image.iloc[rand_number]
 art_price = art_df.price.iloc[rand_number]
-art_low = art_df.low_estimate.iloc[rand_number]
-art_high = art_df.high_estimate.iloc[rand_number]
 art_date = art_df.date.iloc[rand_number]
-art_other = art_df.loc[(art_df['artist'] == artist) & (art_df['price'] != art_price),
-                       'price'].sample(n=1).values[0]
 
-# Initalise global variables
-tolerance = 0.1
-counter = 1
-clues = []
-outcomes = {}
+
+
 
 # Function to provide clues
 def get_clue(data, rand_number, rounds):
+    artist = data.artist.iloc[rand_number]
+    price = data.price.iloc[rand_number]
     # Control flow with match-case 
     match rounds:
         case 1:
-            return f'The art is called: {art_name}'
+            return f'The art is called: {data.title.iloc[rand_number]}'
         case 2:
-            return f'The artist is: {artist}'
-        case 3:
             return f'The auction category is: {data.category.iloc[rand_number]}'
+        case 3:
+             return f'The artist is: {artist}'
         case 4:
-            return f'Other art by this artist has sold for: ${art_other}'
+            art_other = data.loc[(data['artist'] == artist) & (data['price'] != price), 'price'].sample(n=1).values[0]
+            return f'Other art by this artist has auctioned for: ${art_other:,.2f}'
         case 5:
-            return f'The auction price was estimated to be between ${art_low} and ${art_high}'
+            art_low = art_df.low_estimate.iloc[rand_number]
+            art_high = art_df.high_estimate.iloc[rand_number]
+            return f'The auction price was estimated to be between ${art_low:,.2f} and ${art_high:,.2f}'
+
+
+
 
 @app.route('/')
 def index():
@@ -63,7 +71,9 @@ def index():
         # Get clue information
         for i in range(5):
             clues.append(get_clue(art_df, rand_number, (i + 1)))
-        return render_template('over.html', art_name=art_name, art_url=art_url, clues=clues, art_price=art_price, art_date=art_date)
+        return render_template('over.html', result='Game Over!', art_name=art_name, art_url=art_url, clues=clues, art_price=art_price, art_date=art_date, guess=outcomes[max(outcomes.keys())]["guess"])
+
+
 
 
 @app.route('/guess', methods=['POST'])
@@ -84,17 +94,19 @@ def guess():
         except ValueError:
             return redirect('/')
         
+        # Check if guess is within +/- tolerance of the actual price
         if art_price * (1 - tolerance) <= guess <= art_price * (1 + tolerance):
-            information = []
+            clues.clear()
             for i in range(5):
-                information.append(get_clue(art_df, rand_number, (i + 1)))
-            return render_template('winner.html', art_name=art_name, art_url=art_url, information=information, art_price=art_price, art_date=art_date, guess=guess)
+                clues.append(get_clue(art_df, rand_number, (i + 1)))
+            return render_template('over.html', result='Correct!', art_name=art_name, art_url=art_url, clues=clues, art_price=art_price, art_date=art_date, guess=guess)
             
         elif guess > art_price:
             result = 'Too High'
         elif guess < art_price:
             result = 'Too Low'
-            
+        
+        # Store guess and result
         outcomes[counter] = {"guess": guess, "result": result}
 
         # Increment the counter
